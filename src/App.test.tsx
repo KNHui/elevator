b56b6075f9d42fc97a78528ba3e4b492d27a058d
NamHui Kim <rlasoaen@naver.com>
2023-04-07 18:35:17 +0900

    feat: Add components to App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,25 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/React WEB/);
   expect(linkElement).toBeInTheDocument();
+});
+
+
+test("renders a nav element", () => {
+  render(<App />);
+  const navElement = screen.getByRole("navigation");
+  expect(navElement).toBeInTheDocument();
+});
+
+test('should have 3 li tags inside nav', () => {
+  render(<App />);
+  const listItems = screen.getAllByRole('listitem');
+  expect(listItems.length).toBe(3);
+});
+
+test('Article tag should have an h1 tag with text "Welcome"', () => {
+  render(<App />);
+  const headingElement = screen.getByRole('heading', { name: 'Welcome' });
+  expect(headingElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
-function App() {
+function Header(): JSX.Element {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>
+        <a href="/">
+          React WEB
+        </a>
+      </h1>
+    </header>
+  );
+}
+
+function Navigation(): JSX.Element {
+  return (
+    <nav>
+      <ul>
+        <li> <a href="/read/1">html</a> </li>
+        <li> <a href="/read/2">scss</a> </li>
+        <li> <a href="/read/3">js</a> </li>
+      </ul>
+    </nav>
+  );
+}
+
+function Article(): JSX.Element {
+  return (
+    <article>
+      <h1>Welcome</h1>
+    </article>
+  );
+}
+
+function App(): JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Navigation></Navigation>
+      <Article></Article>
     </div>
   );
 }
